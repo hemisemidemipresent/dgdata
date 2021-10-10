e9ad97289d63d51ae3f0b57b4f612766b6fe8477
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-10-10 12:56:59 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,18 @@
 import requests
+import json
 
 
 def decode(data):
-
-    protoBuff = []
-
+    data = list(data)
     for i in range(len(data)):
-        protoBuff.append(data[i])
-
-    for i in range(len(protoBuff)):
-        protoBuff[i] = protoBuff[i] - 21
-        protoBuff[i] = protoBuff[i] - ((i - 14) % 6)
-    res = ''
-    for i in range(len(protoBuff)):
-        res += chr(protoBuff[i])
-    return res
+        data[i] = data[i] - 21
+        data[i] = chr(data[i] - ((i - 14) % 6))
+    return ''.join(data)
 
 
 data = requests.get(
     'https://priority-static-api.nkstatic.com/storage/static/multi?appid=11&files=races/Exponential_4')
-print(data.content)
-print(decode(data.content))
+buffer = data.content
+decoded = decode(buffer)
+obj = json.loads(decoded)
+print(obj)
